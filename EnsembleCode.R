a1b676ad8f1b79641537d9857e37f0b96709706c
Anam Ahsan <146958428+anamahsann@users.noreply.github.com>
2023-10-07 14:28:32 -0400

    Create EnsembleCode.R

---
+++ EnsembleCode.R
@@ -0,0 +1,6 @@
+ensembl=useMart("ENSEMBL_MART_ENSEMBL",dataset="hsapiens_gene_ensembl", 
+                host="https://www.ensembl.org")
+ensembl = useDataset("hsapiens_gene_ensembl",mart=ensembl)
+getBM(attributes=c("ensembl_gene_id", "hgnc_symbol","entrezgene_id"), 
+      filters="mim_morbid_accession", values="603218,604802,143100,606438,607136", 
+      ensembl)
